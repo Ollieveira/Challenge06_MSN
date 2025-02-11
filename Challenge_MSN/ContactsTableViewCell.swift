023c1c23aae54f11a79a5114907e970d48c40b08
amandabaiao <amandadbaiao@gmail.com>
2025-02-11 17:44:59 -0300

    XiB relativo ao componente da lista de contatos

---
+++ Challenge_MSN/ContactsTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  ContactsTableViewCell.swift
+//  Challenge_MSN
+//
+//  Created by Amanda Baião on 11/02/25.
+//
+
+import UIKit
+
+class ContactsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var namelabel: UILabel!
+    
+    @IBOutlet weak var statusLabelView: UILabel!
+    @IBOutlet weak var statusImageView: UIImageView!
+    @IBOutlet weak var dailyMessageLabel: UILabel!
+    @IBOutlet weak var profileImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Challenge_MSN/ViewController.swift
+++ Challenge_MSN/ViewController.swift
@@ -7,10 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        8
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myID", for: indexPath)
+        as! ContactsTableViewCell
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        let xib = UINib(nibName: "ContactsTableViewCell", bundle: nil)
+        tableView.register(xib, forCellReuseIdentifier: "myID")
         // Do any additional setup after loading the view.
     }
 
